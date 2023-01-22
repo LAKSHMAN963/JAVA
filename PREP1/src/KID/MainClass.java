6f44bef9cb4a8d405163ec6721c7ba33526ab654
Lakshman Akula <LAKSHMAN963>
2023-01-21 20:26:32 -0600

    first push

---
+++ PREP1/src/KID/MainClass.java
@@ -0,0 +1,27 @@
+package KID;
+
+import java.util.Scanner;
+
+class Main {
+
+	public void method1() {
+		System.out.println("method 1");
+	}
+
+	public int wrong(int input) {
+		return input * 4;
+	}
+}
+
+public class MainClass {
+	public static void main(String args[]) {
+		Main ex1 = new Main();
+		ex1.method1();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter value");
+		int a = scan.nextInt();
+		Main ex2 = new Main();
+		System.out.println(ex2.wrong(a));
+		scan.close();
+	}
+}
